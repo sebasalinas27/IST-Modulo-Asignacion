48b437c56744c5670fcd908df393bb2ad362f95d
sebasalinas27 <sebastiansalinas.m@icloud.com>
2025-04-11 12:59:57 -0400

    app.py

--- app.py
+++ app.py
@@ -100,6 +100,7 @@ if uploaded_file:
                                 asignado = min(pendiente, stock_disp)
                                 df_asignacion.at[(mes, codigo), cliente] += asignado
                                 df_stock_filtrado.at[(mes, codigo), 'Stock Restante'] -= asignado
+                                if idx in df_minimos.index:
                                 df_minimos.loc[idx, "Pendiente"] -= asignado
 
                 df_asignacion_reset = df_asignacion.reset_index().melt(id_vars=["MES", "Codigo"], var_name="Cliente", value_name="Asignado")
